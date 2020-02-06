b3ec69569ec50b735007c9f6a075c2b14aa8370a
drojilla <drojilla@gmail.com>
2020-02-07 00:13:16 +0100

    WIP katas

--- refactoring-to-hexagonal-architecture/src/BirthdayService.php
+++ refactoring-to-hexagonal-architecture/src/BirthdayService.php
@@ -8,7 +8,50 @@ namespace App;
 
 class BirthdayService
 {
+    public function sendGreetings(
+        string $fileName,
+        OurDate $ourDate,
+        string $smtpHost,
+        int $smtpPort
+    ): void
+    {
+        $fileHandler = fopen($fileName, 'rb');
+        fgetcsv($fileHandler);
+        while ($employeeData = fgetcsv($fileHandler, null)) {
+            $employeeData = array_map('trim', $employeeData);
+            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+            if ($employee->isBirthday($ourDate)) {
+                $recipient = $employee->getEmail();
+                $body = sprintf('Happy Birthday, dear %s!', $employee->getFirstName());
+                $subject = 'Happy Birthday!';
+                $this->sendMessage($smtpHost, $smtpPort, 'sender@here.com', $subject, $body, $recipient);
+            }
+        }
+    }
 
+    private function sendMessage(
+        string $smtpHost,
+        int $smtpPort,
+        string $sender,
+        string $subject,
+        string $body,
+        string $recipient
+    ): void
+    {
 
+//        // Create a mailer
+//        $mailer = new Swift_Mailer(
+//            new Swift_SmtpTransport($smtpHost, $smtpPort)
+//        );
+//        // Construct the message
+//        $msg = new Swift_Message($subject);
+//        $msg
+//            ->setFrom($sender)
+//            ->setTo([$recipient])
+//            ->setBody($body)
+//        ;
+//        // Send the message
+//        $mailer->send($msg);
+    }
 
 }
---
+++ refactoring-to-hexagonal-architecture/tests/AcceptanceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+
+use App\BirthdayService;
+use App\OurDate;
+use PHPUnit\Framework\TestCase;
+
+class AcceptanceTest extends TestCase
+{
+    const SMTP_PORT = 25;
+    private $service;
+
+    public function setUp(): void
+    {
+        $this->service = new BirthdayService();
+    }
+
+    public function testWillNotSendEmailsWhenNobodysBirthday()
+    {
+        $this->service->sendGreetings(
+            dirname(__FILE__) . "/resources/employee_data.txt",
+            new OurDate("2008/01/01"),
+            "localhost",
+            self::SMTP_PORT);
+        $this->assertEquals(
+            $messagesSent->size(),
+            0,
+            "what? messages?");
+    }
+
+}
