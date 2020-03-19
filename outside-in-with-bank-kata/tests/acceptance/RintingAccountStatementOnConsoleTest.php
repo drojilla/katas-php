3c9849f8cf51e9b2f3cad819f266fa502f1d6d80
drojilla <drojilla@gmail.com>
2020-03-19 21:55:20 +0100

    wip

---
+++ outside-in-with-bank-kata/tests/acceptance/RintingAccountStatementOnConsoleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class PrintingAccountStatementOnConsoleTest extends TestCase
+{
+    public function testPrintingStatementIncludingDepositAndWithDrawal()
+    {
+        $this->assertTrue(false);
+    }
+}
