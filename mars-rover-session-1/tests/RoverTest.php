7ea0b2a927b68ea7ca20c146d4f13a49885776c2
drojilla <drojilla@gmail.com>
2020-03-22 20:17:14 +0100

    wip

--- mars-rover-session-1/tests/RoverTest.php
+++ mars-rover-session-1/tests/RoverTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-class FakeTest extends TestCase
+class RoverTest extends TestCase
 {
     public function testFails()
     {
---
+++ mars-rover-session-2/src/Rover.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class Rover
+{
+    private string $direction;
+    private int $y;
+    private int $x;
+
+    public function __construct(int $x, int $y, string $direction)
+    {
+        $this->direction = $direction;
+        $this->y = $y;
+        $this->x = $x;
+    }
+
+    public function y(): int
+    {
+        return $this->y;
+    }
+
+    public function x(): int
+    {
+        return $this->x;
+    }
+
+    public function direction(): string
+    {
+        return $this->direction;
+    }
+
+    public function receive(string $commandsSequence): void
+    {
+        $commandsSequenceLenght = strlen($commandsSequence);
+        for ($i = 0; $i < $commandsSequenceLenght; ++$i) {
+            $command = substr($commandsSequence, $i, $i + 1);
+            if ($command === "l" || $command === "r") {
+                // Rotate Rover
+                if ($this->direction() === "N") {
+                    if ($command === "r") {
+                        $this->direction = "E";
+                    } else {
+                        $this->direction = "W";
+                    }
+                } else if ($this->direction() === "S") {
+                    if ($command === "r") {
+                        $this->direction = "W";
+                    } else {
+                        $this->direction = "E";
+                    }
+                } else if ($this->direction() === "W") {
+                    if ($command === "r") {
+                        $this->direction = "N";
+                    } else {
+                        $this->direction = "S";
+                    }
+                } else {
+                    if ($command === "r") {
+                        $this->direction = "S";
+                    } else {
+                        $this->direction = "N";
+                    }
+                }
+            } else {
+                // Displace Rover
+                $displacement1 = -1;
+
+                if ($this->direction() === "f") {
+                    $displacement1 = 1;
+                }
+                $displacement = $displacement1;
+
+                if ($this->direction() === "N") {
+                    $this->y += $displacement;
+                } else if ($this->direction() === "S") {
+                    $this->y -= $displacement;
+                } else if ($this->direction() === "W") {
+                    $this->x -= $displacement;
+                } else {
+                    $this->x += $displacement;
+                }
+            }
+        }
+    }
+
+    public function equals(Rover $rover): bool
+    {
+        if ($this->y() !== $rover->y()) {
+            return false;
+        }
+        if ($this->x() !== $rover->x()) {
+            return false;
+        }
+        return $this->direction() !== null
+            ? $this->direction() === $rover->direction()
+            : $rover->direction() === null;
+    }
+}
