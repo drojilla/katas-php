e87d6f0455fc247019f9a10526d1cc33b473d9ee
drojilla <drojilla@gmail.com>
2020-03-22 20:53:36 +0100

    wip

---
+++ mars-rover-session-2/tests/RoverReceivingCommandsListTests.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Rover;
+use PHPUnit\Framework\TestCase;
+
+class RoverReceivingCommandsListTests
+{
+    public function testNoCommands(): void
+    {
+        $rover = new Rover(0, 0, "N");
+
+        $rover->receive("");
+
+        $this->assertEquals(new Rover(0, 0, "N"), $rover);
+    }
+
+    public function testTwoCommands(): void
+    {
+        $rover = new Rover(0, 0, "N");
+
+        $rover->receive("lf");
+
+        $this->assertEquals(new Rover(-1, 0, "W"), $rover);
+    }
+
+    public function testManyCommands(): void
+    {
+        $rover = new Rover(0, 0, "N");
+
+        $rover->receive("ffrbbrfflff");
+
+        $this->assertEquals(new Rover(0, 0, "E"), $rover);
+    }
+
+}
