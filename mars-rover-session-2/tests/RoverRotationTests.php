a388787facb7534e2176cf71967f6b7f5bbcf4e2
drojilla <drojilla@gmail.com>
2020-03-22 21:30:22 +0100

    wip

---
+++ mars-rover-session-2/tests/RoverRotationTests.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Rover;
+use PHPUnit\Framework\TestCase;
+
+class RoverRotationTests extends TestCase
+{
+    public function testFacingNorthRotateLeft()
+    {
+        $rover = new Rover(0, 0, "N");
+
+        $rover->receive("l");
+
+        $this->assertEquals(new Rover(0, 0, "W"), $rover);
+    }
+
+    public function testFacingNorthRotateRight()
+    {
+        $rover = new Rover(0, 0, "N");
+
+        $rover->receive("r");
+
+        $this->assertEquals(new Rover(0, 0, "E"), $rover);
+    }
+
+    public function testFacingSouthRotateLeft()
+    {
+        $rover = new Rover(0, 0, "S");
+
+        $rover->receive("l");
+
+        $this->assertEquals(new Rover(0, 0, "E"), $rover);
+    }
+
+    public function testFacingSouthRotateRight()
+    {
+        $rover = new Rover(0, 0, "S");
+
+        $rover->receive("r");
+
+        $this->assertEquals(new Rover(0, 0, "W"), $rover);
+    }
+
+    public function testFacingWestRotateLeft()
+    {
+        $rover = new Rover(0, 0, "W");
+
+        $rover->receive("l");
+
+        $this->assertEquals(new Rover(0, 0, "S"), $rover);
+    }
+
+    public function testFacingWestRotateRight()
+    {
+        $rover = new Rover(0, 0, "W");
+
+        $rover->receive("r");
+
+        $this->assertEquals(new Rover(0, 0, "N"), $rover);
+    }
+
+    public function testFacingEastRotateLeft()
+    {
+        $rover = new Rover(0, 0, "E");
+
+        $rover->receive("l");
+
+        $this->assertEquals(new Rover(0, 0, "N"), $rover);
+    }
+
+    public function testFacingEastRotateRight()
+    {
+        $rover = new Rover(0, 0, "E");
+
+        $rover->receive("r");
+
+        $this->assertEquals(new Rover(0, 0, "S"), $rover);
+    }
+}
