4a5d1fea705c2f0750cf5b585040445089957a05
drojilla <drojilla@gmail.com>
2020-03-22 20:36:17 +0100

    wip

---
+++ mars-rover-session-2/tests/RoverEqualityTests.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Rover;
+use PHPUnit\Framework\TestCase;
+
+class RoverEqualityTests extends TestCase
+{
+    public function testEqualRovers(): void
+    {
+        $this->assertEquals(new Rover(1, 1, "N"), new Rover(1, 1, "N"));
+    }
+
+    public function notEqualRovers(): void
+    {
+        $this->assertFalse((new Rover(1, 1, "N"))->equals(new Rover(1, 1, "S")));
+        $this->assertFalse((new Rover(1, 1, "N"))->equals(new Rover(1, 2, "N")));
+        $this->assertFalse((new Rover(1, 1, "N"))->equals(new Rover(0, 1, "N")));
+    }
+}
